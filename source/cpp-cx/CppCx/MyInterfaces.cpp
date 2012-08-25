9116475b7584eeab6e9a3b4d2bf7a15b34b141bd
Michael B. Price <michael.b.price@gmail.com>
2012-08-25 09:11:52 -0500

    Adding interface example

---
+++ source/cpp-cx/CppCx/MyInterfaces.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "MyInterfaces.h"
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+using namespace Platform;
+
+void MyImplementation::Finish (String ^ id)
+{
+    m_completed->Append(id);
+
+    OnFinished(id);
+}
+
+bool MyImplementation::IsFinished (String ^ id)
+{
+    auto result = find(begin(m_completed), end(m_completed), id);
+
+    return result != end(m_completed);
+}
---
+++ source/cpp-cx/CppCx/MyInterfaces.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef INCL_MYINTERFACES_H
+#define INCL_MYINTERFACES_H
+
+#include <collection.h>
+
+delegate void FinishedEventHandler (Platform::String ^ id);
+
+interface class IMyInterface
+{
+    void Finish (Platform::String ^id);
+
+    bool IsFinished (Platform::String ^ id);
+
+    event FinishedEventHandler ^ OnFinished;
+};
+
+interface class IMyDerivedInterface : public IMyInterface
+{
+    property Platform::String ^ Name;
+};
+
+interface class ISomeOtherInterface
+{
+};
+
+ref class MyImplementation : public IMyDerivedInterface, public ISomeOtherInterface
+{
+public:
+    virtual property Platform::String ^ Name;
+    virtual void Finish (Platform::String ^ id);
+    virtual bool IsFinished (Platform::String ^ id);
+    virtual event FinishedEventHandler ^ OnFinished;
+
+private:
+    Platform::String ^ m_name;
+    Platform::Collections::Vector<Platform::String ^> ^ m_completed;
+};
+
+#endif // INCL_MYINTERFACES_H
