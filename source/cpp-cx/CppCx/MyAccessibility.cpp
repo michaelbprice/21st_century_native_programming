f94919e2f4fae0bb6107f6b0ce53113a35f6c8b1
Michael B. Price <michael.b.price@gmail.com>
2012-08-26 12:23:58 -0500

    Adding namespace/accessibility examples

---
+++ source/cpp-cx/CppCx/MyAccessibility.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "MyAccessibility.h"
+#include <string>
+
+namespace MyNamespace {
+
+using namespace std;
+using namespace Platform;
+
+MyPublicType::MyPublicType (String ^ name)
+    : m_defaultName(name->Data(), name->Length())
+    , m_nameForMyChildren(L"Papa Bear")
+{ }
+
+String ^ MyPublicType::GetDefaultName ()
+{
+    return ref new String(m_defaultName.c_str(), m_defaultName.length());
+}
+
+void MyPublicType::SetDefaultName (String ^ name)
+{
+    m_defaultName = wstring(name->Data(), name->Length());
+}
+
+const std::wstring & MyPublicType::GetInternalNameThatCSharpCannot ()
+{
+    return m_defaultName;
+}
+
+Platform::String ^ MyPublicType::GetNameForChildren ()
+{
+    return ref new String(m_nameForMyChildren.c_str(), m_nameForMyChildren.length());
+}
+
+
+} // namespace MyNamespace
---
+++ source/cpp-cx/CppCx/MyAccessibility.h
@@ -0,0 +1,78 @@
+#ifndef INCL_MYACCESSIBILITY_H
+#define INCL_MYACCESSIBILITY_H
+
+#include <string>
+
+struct SomeCppType
+{ };
+
+namespace MyNamespace {
+
+// Will generate WinRT metadata
+//
+public ref class MyPublicType
+{
+
+// Public C++ accessibility, public WinRT metadata
+// *** Normal C++ types not allowed ***
+//
+public:
+    //wchar_t * GetDefaultName (); // error C3992: 'GetDefaultName': signature of public member contains invalid type 'wchar_t *'
+    Platform::String ^ GetDefaultName ();
+
+
+// Protected C++ accessibility, protected WinRT metadata
+// *** Normal C++ types not allowed ***
+//
+protected:
+    void SetDefaultName (Platform::String ^ name);
+
+
+// Private C++ accessibility, no WinRT metadata
+// *** Normal C++ types allowed ***
+//
+private:
+    std::wstring m_defaultName;
+    MyPublicType (Platform::String ^ name);
+
+
+// ********************* HERE'S WHERE IT GETS COMPLICATED *********************
+
+
+// Public C++ accessibility, no WinRT metadata
+// *** Normal C++ types allowed ***
+//
+internal: // or private public:
+    const std::wstring & GetInternalNameThatCSharpCannot ();
+
+
+// Public C++ accessibility, protected WinRT metadata
+// *** Normal C++ types not allowed ***
+//
+public protected: // or protected public:
+    Platform::String ^ GetNameForChildren ();
+
+
+// Protected C++ accessibility, no WinRT metadata
+// *** Normal C++ types allowed ***
+//
+protected private: // or private protected:
+    std::wstring m_nameForMyChildren;
+};
+
+// No WinRT metadata
+//
+private ref class MyPrivateType // or no accessibility modifier
+{
+public:
+    //SomeCppType PublicGetCppType ();
+
+};
+
+} // namespace MyNamespace
+
+
+public ref class MyInvalidType
+{ };
+
+#endif // INCL_MYACCESSIBILITY_H
