9524d84a6248e8004da26eeee3c77cca2039f997
Michael B. Price <michael.b.price.dev@gmail.com>
2012-09-03 09:21:37 -0700

    Exercise 8

---
+++ course/cpp11/exercise-8/Logger.hpp
@@ -0,0 +1,51 @@
+#ifndef INCL_LOGGER_HPP__
+#define INCL_LOGGER_HPP__
+
+#include <iostream>
+#include <utility>
+
+namespace Go {
+
+enum class LogLevel : unsigned char
+{
+    kNone = 0,
+    kLow = 1,
+    kMedium = 2,
+    kHigh = 3,
+    kFirehose = 4
+};
+
+class Logger
+{
+ private:
+    LogLevel m_level = LogLevel::kNone;
+
+ public:
+    Logger (LogLevel level)
+      : m_level(level)
+    { }
+
+    template <typename T>
+    void log (LogLevel level, T && t)
+    {    
+        if (level <= m_level)
+        {
+            std::cout << "LOG(" << static_cast<unsigned short>(level) << "): ";
+            doLog(level, std::forward<T>(t));
+        }
+    }
+
+ private:
+
+    template <typename T>
+    void doLog (LogLevel level, T && t)
+    {
+        std::cout << t << std::endl; 
+    }
+};
+
+} // namespace Go
+
+extern Go::Logger gLogger;
+
+#endif /* end of include guard: INCL_LOGGER_HPP__ */
