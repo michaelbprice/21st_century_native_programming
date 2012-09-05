e34ac31b35c4c56ad99545ae73fc20e1f82b414d
Michael B. Price <michael.b.price@gmail.com>
2012-09-04 23:10:51 -0500

    Finessing the value type unit tests

--- source/cpp-cx/CppCxTest/TestValueType.cpp
+++ source/cpp-cx/CppCxTest/TestValueType.cpp
@@ -18,5 +18,17 @@ namespace CppCxTest
             Assert::AreEqual("Hello", v.m_string);
 		}
 
+        TEST_METHOD(TestCopySemantics)
+        {
+            MyValueType one = {1, "Hello"};
+            MyValueType two = one;
+
+            two.m_integer = 0;
+            two.m_string = "World";
+
+            Assert::AreNotEqual(one.m_integer, two.m_integer);
+            Assert::AreNotEqual(one.m_string, two.m_string);
+        }
+
 	};
 }
